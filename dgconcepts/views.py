989c965c25ddc1bf085ba608c19b94a716120837
Alina Kramchaninova <alina.kramchaninova@crosslang.com>
2020-06-05 10:42:10 +0200

    amended terms and views

--- dgconcepts/views.py
+++ dgconcepts/views.py
@@ -18,15 +18,36 @@ from .serializers import TermModelSerializer
 import base64
 
 class TermView ( APIView):
-    
+
     #for a single text doc
-    def post(self, request): # here we should also add pd_2 as input
+    def post(self, request): 
+        corpus_table = pd.read_csv('/path/to/the/file.csv')
+    
         doc_id = 1 # should be unique for each input, to be amended
-        f = request.data
+        f = request.data # the input is a json with 'content' and 'content_type'
+        if f['content_type'] == 'html':
+            decoded_content = base64.b64decode(f['content']).decode('utf-8')
+            decoded_content = Cleaner(style=True, links=True, add_nofollow=True, page_structure=False, safe_attrs_only=False).clean_html(decoded_content)
+            raw_text = html.fromstring(decoded_content)
+            raw_text = [s for s in raw_text.text_content().splitlines() if s]
+    
+        if f['content_type'] == 'xhtml':
+            decoded_content = base64.b64decode(f['content'])
+            decoded_content = Cleaner(style=True, links=True, add_nofollow=True, page_structure=False, safe_attrs_only=False).clean_html(decoded_content)
+            raw_text = html.fromstring(decoded_content)
+            raw_text = [s for s in raw_text.text_content().splitlines() if s]
+        
+        if f['content_type'] == 'pdf':
+            decoded_content = base64.b64decode(f['content']).decode('utf-8')
+            raw_text = [s for s in decoded_content.splitlines() if s]
+        
+        """
         decoded_content = base64.b64decode(f['content']).decode('utf-8')
         decoded_content = Cleaner(style=True, links=True, add_nofollow=True, page_structure=False, safe_attrs_only=False).clean_html(decoded_content)
         raw_text = html.fromstring(decoded_content)
         raw_text = [s for s in raw_text.text_content().splitlines() if s]
+        """
+        
         lowerTexts = []
         for x in raw_text:
             lowerTexts.append(x.lower())
@@ -36,9 +57,14 @@ class TermView ( APIView):
         for x in list(zip(dict_1['term_ngram'], dict_1['count'])):
             list_of_terms.append(x)
         #pandas_dataframe_1 = pd.DataFrame.from_dict(dict_1) #(id_doc | term x-gram | count x-gram | tf )
-        #pandas_dataframe_2 = pd.DataFrame.from_dict(dict_2) # literally all ngrams, not just terms
+        pandas_dataframe_2 = pd.DataFrame.from_dict(dict_2) # literally all ngrams, not just terms
+        corpus_table = corpus_table.append(pandas_dataframe_2)
+        aggregation_functions = {'df': 'sum'}
+        corpus_table = corpus_table.groupby(corpus_table['ngrams']).aggregate(aggregation_functions)
+        corpus_table.to_csv('/path/to/the/file.csv')
+        
         return HttpResponse(str(list_of_terms))
-    
+        
     """
     def post(self, request):
     #for multiple text docs
