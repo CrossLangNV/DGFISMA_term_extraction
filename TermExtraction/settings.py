9afb92df76b2129f471557f33153c11e91b4f100
Oan Stultjens <oan.stultjens@crosslang.com>
2020-09-10 11:59:44 +0200

    Changed ALLOWED HOSTS

--- TermExtraction/settings.py
+++ TermExtraction/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = '***REMOVED***'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [h for h in os.environ.get(
+    'DJANGO_ALLOWED_HOSTS', "localhost django").split(" ")]
 
 
 # Application definition
