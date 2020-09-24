020fab4169bc0bcb6016c9930765cd7510ec79f4
Alina Kramchaninova <alina.kramchaninova@crosslang.com>
2020-09-24 14:38:30 +0200

    adding lemmata

--- dgconcepts/pipeline/annotations.py
+++ dgconcepts/pipeline/annotations.py
@@ -4,6 +4,7 @@ from cassis import Cas, TypeSystem
 import ahocorasick as ahc
 import os
 import pandas as pd
+import spacy
 from django.conf import settings
 
 
@@ -12,8 +13,10 @@ def add_terms_to_cas(cas: Cas, typesystem: TypeSystem, SofaID: str, terms_tf_idf
     '''
     Given a cas and its typesystem, this function adds terms and associated tf_idf score (terms_tf_idf) to a given view (SofaID) as type.Tfidf. Annotations will only be added to ValueBetweenTagType elements with tag.tagName in the set tagnames. Returns the same cas object as the input cas, but now with annotations added.
     '''
-
+    nlp = spacy.load('en_core_web_sm')
     Token = typesystem.get_type('de.tudarmstadt.ukp.dkpro.core.api.frequency.tfidf.type.Tfidf')
+    Lemma = typesystem.get_type('de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Lemma')
+    
     A = ahc.Automaton()
     cas_view = cas.get_view(SofaID)
     for (key, cat) in terms_tf_idf:
@@ -25,9 +28,15 @@ def add_terms_to_cas(cas: Cas, typesystem: TypeSystem, SofaID: str, terms_tf_idf
             text = tag.get_covered_text()
             try:
                 for end_index, (tfidf, term) in A.iter(text):
+                    lemmas = []
+                    for word in nlp(term):
+                        lemmas.append(word.lemma_)
+                    term_lemmas = ' '.join(lemmas)
+
                     start_index = end_index - len(term) + 1
                     cas_view.add_annotation(
                         Token(begin=tag.begin + start_index, end=tag.begin + end_index + 1, tfidfValue=tfidf, term=term))
+                    cas_view.add_annotation(Lemma(begin=tag.begin + start_index, end=tag.begin + end_index + 1, value=term_lemmas))
             except:
                 continue
     return cas
