23960d054f811959c1e3f72b0bf309d425814078
Alina Kramchaninova <alina.kramchaninova@crosslang.com>
2020-10-23 19:44:19 +0200

    revising annotations

--- dgconcepts/pipeline/annotations.py
+++ dgconcepts/pipeline/annotations.py
@@ -1,8 +1,12 @@
 import ahocorasick as ahc
 from cassis import Cas, TypeSystem
 from typing import List, Tuple, Set
-import string
 import re
+import string
+
+TFIDF_FOR_WHITELISTED_SUBJECTS = -2
+TFIDF_FOR_ANNOTATED_SUBJECTS = -1
+
 
 def proceed_with_annotation(start_index, end_index, term, text):
     proceed_with_annotation = False
@@ -25,15 +29,17 @@ def proceed_with_annotation(start_index, end_index, term, text):
 
     return proceed_with_annotation
 
-def add_terms_and_lemmas_to_cas(NLP, cas: Cas, typesystem: TypeSystem, SofaID: str, terms_tf_idf: List[Tuple[str, float]],
-                     tagnames: Set[str] = set('p')) -> Cas:
+
+def add_terms_and_lemmas_to_cas(NLP, cas: Cas, typesystem: TypeSystem, SofaID: str,
+                                terms_tf_idf: List[Tuple[str, float]],
+                                tagnames: Set[str] = set('p')) -> Cas:
     '''
     Given a cas and its typesystem, this function adds terms and associated tf_idf score (terms_tf_idf) to a given view (SofaID) as type.Tfidf. Annotations will only be added to ValueBetweenTagType elements with tag.tagName in the set tagnames. Returns the same cas object as the input cas, but now with annotations added.
     NLP is the SpaCy model for the extraction of lemmas per token
     '''
     Token = typesystem.get_type('de.tudarmstadt.ukp.dkpro.core.api.frequency.tfidf.type.Tfidf')
     Lemma = typesystem.get_type('de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Lemma')
-    
+
     A = ahc.Automaton()
     cas_view = cas.get_view(SofaID)
     for (key, cat) in terms_tf_idf:
@@ -43,8 +49,10 @@ def add_terms_and_lemmas_to_cas(NLP, cas: Cas, typesystem: TypeSystem, SofaID: s
     for tag in cas_view.select("com.crosslang.uimahtmltotext.uima.type.ValueBetweenTagType"):
         conditionMet = False
         if tag.tagName in set(tagnames):
-            if all(x.tagName in set(tagnames) for x in list(cas.get_view(SofaID).select_covered("com.crosslang.uimahtmltotext.uima.type.ValueBetweenTagType", tag))):
-                    conditionMet = True
+            if all(x.tagName in set(tagnames) for x in list(
+                    cas.get_view(SofaID).select_covered("com.crosslang.uimahtmltotext.uima.type.ValueBetweenTagType",
+                                                        tag))):
+                conditionMet = True
         if conditionMet:
             text = tag.get_covered_text().lower()
             for end_index, (tfidf, term) in A.iter(text):
@@ -55,11 +63,14 @@ def add_terms_and_lemmas_to_cas(NLP, cas: Cas, typesystem: TypeSystem, SofaID: s
                         lemmas.append(word.lemma_)
                     term_lemmas = ' '.join(lemmas)
                     cas_view.add_annotation(
-                        Token(begin=tag.begin + start_index, end=tag.begin + end_index + 1, tfidfValue=tfidf, term=term))
-                    cas_view.add_annotation(Lemma(begin=tag.begin + start_index, end=tag.begin + end_index + 1, value=term_lemmas))
+                        Token(begin=tag.begin + start_index, end=tag.begin + end_index + 1, tfidfValue=tfidf,
+                              term=term))
+                    cas_view.add_annotation(
+                        Lemma(begin=tag.begin + start_index, end=tag.begin + end_index + 1, value=term_lemmas))
     return cas
 
-def add_checked_term_to_cas(cas_view, sentence, np, TYPESYSTEM):
+
+def add_checked_term_to_cas(cas_view, sentence, np, TYPESYSTEM, tfidf_score):
     A = ahc.Automaton()
     Token = TYPESYSTEM.get_type('de.tudarmstadt.ukp.dkpro.core.api.frequency.tfidf.type.Tfidf')
     A.add_word(np.text, (1, np.text.strip()))
@@ -67,20 +78,65 @@ def add_checked_term_to_cas(cas_view, sentence, np, TYPESYSTEM):
     for end_index, (tfidf, term) in A.iter(sentence.get_covered_text()):
         start_index = end_index - len(term) + 1
         cas_view.add_annotation(
-            Token(begin=sentence.begin + start_index, end=sentence.begin + end_index + 1, tfidfValue=-1,
+            Token(begin=sentence.begin + start_index, end=sentence.begin + end_index + 1, tfidfValue=tfidf_score,
                   term=np.text))
+
+
+def get_definition_subject(NLP, sentence):
+    """
+
+    :param NLP: the SpaCy model
+    :param sentence: object of type "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence"
+    :return: spacy.tokens.span.Span
+    """
+    definition = sentence.get_covered_text()
+    definition = re.sub(r'\(\w{1}\)', '', definition)  # remove (a) or (b) before the definition
+    definition = re.sub(r'\(\d{1, 3}\.\)', '', definition)  # remove 1. or 2. before the definition
+    definition = definition.translate(str.maketrans('', '', string.punctuation.replace('-', '').replace('/', '')))
+
+    definition = definition.strip()
+    doc = NLP(definition)
+    definition_subject = ''
+    for np in doc.noun_chunks:
+        for token in np:
+            if token.dep_ == 'nsubj' and token.head.dep_ == 'ROOT':
+                definition_subject = np
+
+    return definition_subject
 
 
 def check_definitions(cas, NLP, TYPESYSTEM):
     cas_view = cas.get_view("html2textView")
     for sentence in cas_view.select("de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence"):
-        definition = sentence.get_covered_text()
-        definition = re.sub(r'\(\w{1}\)', '', definition)
-        definition = definition.translate(str.maketrans('', '', string.punctuation))
-        definition = definition.strip()
-        doc = NLP(definition)
-        for np in doc.noun_chunks:
-            for token in np:
-                if token.dep_ == 'nsubj' and token.head.dep_ == 'ROOT':
-                    add_checked_term_to_cas(cas_view, sentence, np, TYPESYSTEM)
+        definition_subject = get_definition_subject(NLP, sentence)
+        token_annotations = cas_view.select_covered('de.tudarmstadt.ukp.dkpro.core.api.frequency.tfidf.type.Tfidf',
+                                                  sentence)
+        annotated_terms = [annotation.term for annotation in token_annotations]
+
+        no_terms_were_detected = False
+        subject_was_not_detected = False
+
+        # if no terms were annotated
+        if len(annotated_terms) == 0:
+            no_terms_were_detected = True
+            add_checked_term_to_cas(cas_view, sentence, definition_subject, TYPESYSTEM, TFIDF_FOR_ANNOTATED_SUBJECTS)
+
+        #  subject is not a whitelisted term and has not been annotated
+        if definition_subject.text not in annotated_terms:
+            subject_was_not_detected = True
+            add_checked_term_to_cas(cas_view, sentence, definition_subject, TYPESYSTEM, TFIDF_FOR_ANNOTATED_SUBJECTS)
+
+        if (no_terms_were_detected and subject_was_not_detected) == False:
+            for token in token_annotations:
+                #  subject is a whitelisted term
+                if token.tfidfValue == TFIDF_FOR_ANNOTATED_SUBJECTS and token.term == definition_subject.text:
+                    add_checked_term_to_cas(cas_view, sentence, definition_subject, TYPESYSTEM,
+                                            TFIDF_FOR_WHITELISTED_SUBJECTS)
+
+                # subject is not a whitelisted term and has been annotated
+                if token.tfidfValue != (
+                        TFIDF_FOR_ANNOTATED_SUBJECTS or TFIDF_FOR_WHITELISTED_SUBJECTS) and token.term == definition_subject.text:  # sub
+                    add_checked_term_to_cas(cas_view, sentence, definition_subject, TYPESYSTEM,
+                                            TFIDF_FOR_ANNOTATED_SUBJECTS)
+
     return cas
