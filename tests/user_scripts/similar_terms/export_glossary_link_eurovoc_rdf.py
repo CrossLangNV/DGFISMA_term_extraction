09cce5e178114b23d62b0e0b66e6a64f7069408b
neuro <laurensmeeus93@gmail.com>
2020-12-29 10:19:19 +0100

    proper RDF queries for testing.

--- tests/user_scripts/similar_terms/export_glossary_link_eurovoc_rdf.py
+++ tests/user_scripts/similar_terms/export_glossary_link_eurovoc_rdf.py
@@ -9,7 +9,7 @@ from user_scripts.similar_terms import export_glossary_link_eurovoc_rdf
 
 class TestPipeline(unittest.TestCase):
     def test_full_pipeline(self):
-        l_terms = ['word', 'second', 'secondary', 'word', 'another one', 'economic', 'money']
+        l_terms = ['word', 'second', 'the word', 'secondary', 'word', 'another one', 'economic', 'money']
 
         with tempfile.TemporaryDirectory() as tmp_dir:
             filename_terms = os.path.join(tmp_dir, 'glossary.txt')
@@ -47,3 +47,56 @@ class TestPipeline(unittest.TestCase):
             triple_filtered = [s for s, p, o in triples if str(p) == 'http://www.w3.org/2004/02/skos/core#relatedMatch']
 
             self.assertTrue(len(triple_filtered), 'Did not find related terms in RDF graph')
+
+        # Get all terms
+        q_terms = """
+        prefix skos: <http://www.w3.org/2004/02/skos/core#>
+
+        SELECT ?term
+        WHERE {
+          ?subject skos:prefLabel ?term .
+        }
+        """
+
+        r_labels = set(map(str, [el[0] for el in g.query(q_terms)]))
+
+        with self.subTest('Find terms'):
+            self.assertEqual(r_labels, set(l_terms), 'Should return same terms')
+
+
+        # Get related terms self
+
+        q_self_related = """
+        prefix skos: <http://www.w3.org/2004/02/skos/core#>
+
+        SELECT ?term0 ?term1
+        WHERE {
+            ?subject0 skos:prefLabel ?term0 ;
+                skos:relatedMatch ?subject1 .
+            FILTER ( ?subject0 != ?subject1 )
+            ?subject1 skos:prefLabel ?term1 .
+
+        }
+        """
+
+        r_sim_term = list(g.query(q_self_related))
+
+        with self.subTest('Find related terms'):
+            self.assertTrue(len(r_sim_term), f'Should be non-empty: {r_sim_term}')
+
+
+        q_related_all = """
+        prefix skos: <http://www.w3.org/2004/02/skos/core#>
+
+        SELECT ?term0 ?subject1
+        WHERE {
+            ?subject0 skos:prefLabel ?term0 ;
+                skos:relatedMatch ?subject1 .
+            FILTER ( ?subject0 != ?subject1 )
+        }
+        """
+
+        r_related_all = list(g.query(q_related_all))
+
+        with self.subTest('Find all pairs'):
+            self.assertGreater(len(r_related_all), len(r_sim_term), f'Should find relations with EuroVoc: {r_related_all}')
