6888b6409c20b89e9c55b9da9eaac012c9588ac7
ArneDefauw <arne.defauw@crosslang.com>
2021-04-20 15:54:16 +0200

    Rule based term extraction: correct the offset of the stripped term (strip of whitespace and enumeration) for correct highlighting in solr.

--- dgconcepts/pipeline/rule_based_extraction.py
+++ dgconcepts/pipeline/rule_based_extraction.py
@@ -6,12 +6,16 @@ from typing import Tuple, List
 
 from cassis import Cas, TypeSystem
 
+from .utils import lemmatize
+
+from spacy.lang.en import English
+
 '''
 Module to perform rule based term and definition extraction from tables.
 Terms and definitions are extracted from eur-lex files with table header: 'Legal references and instructions'.
 '''
 
-def rule_based_concept_extraction( cas: Cas, typesystem: TypeSystem, config: ConfigParser ):
+def rule_based_concept_extraction( NLP: English, cas: Cas, typesystem: TypeSystem, config: ConfigParser ):
     
     SofaID = config[ 'Annotation' ].get( 'SOFA_ID' )
     Value_between_tagtype = config[ 'Annotation' ].get( 'VALUE_BETWEEN_TAG_TYPE' )
@@ -24,10 +28,10 @@ def rule_based_concept_extraction( cas: Cas, typesystem: TypeSystem, config: Con
                     #if one of the p's inside the tbody tag contains "Legal references and instructions", then the tbody tag is one of the tables
                     #to process via rule based system                    
                     remove_terms_and_definitions_ml( cas, typesystem, config, tag  )
-                    get_terms_and_definitions( cas, typesystem, config, tag )
+                    get_terms_and_definitions( NLP, cas, typesystem, config, tag )
                     break
 
-def get_terms_and_definitions(cas: Cas, typesystem: TypeSystem, config: ConfigParser, table_tag):
+def get_terms_and_definitions(NLP: English, cas: Cas, typesystem: TypeSystem, config: ConfigParser, table_tag):
     
     SofaID = config[ 'Annotation' ].get( 'SOFA_ID' )
     Value_between_tagtype = config[ 'Annotation' ].get( 'VALUE_BETWEEN_TAG_TYPE' )
@@ -39,7 +43,12 @@ def get_terms_and_definitions(cas: Cas, typesystem: TypeSystem, config: ConfigPa
                 if subtag.tagName == 'p' and "class='ti-grseq-1'" in subtag.attributes:
                     #strip enumeration from start of a term
                     stripped_term=clean_term( subtag.get_covered_text() )
-                    term = ( stripped_term , subtag.begin, subtag.end )
+                    stripped_term=stripped_term.lower()
+                    #search for offset of stripped_term in sofa:
+                    offset_stripped_term=get_offset_stripped_term( stripped_term, subtag.get_covered_text() )
+                    if not offset_stripped_term:
+                        continue
+                    term = ( stripped_term , subtag.begin+offset_stripped_term[0], subtag.begin + offset_stripped_term[1] )
                     definition = ( tag.get_covered_text().strip(), tag.begin, tag.end )
                     if not stripped_term.lower().startswith('of which'):
                         term_cache.append( stripped_term )
@@ -47,7 +56,7 @@ def get_terms_and_definitions(cas: Cas, typesystem: TypeSystem, config: ConfigPa
                         if term_cache:
                             term_text=term_cache[-1]+ ' '+term[0]
                             term=( term_text , term[1], term[2]  )
-                    annotate_terms_and_definitions( cas, typesystem, config, (term, definition) )
+                    annotate_terms_and_definitions( NLP, cas, typesystem, config, (term, definition) )
                     #break, because assume only one term (with class='ti-grseq-1' attribute ) in each 'td' tag
                     break
 
@@ -74,19 +83,21 @@ def remove_terms_and_definitions_ml( cas: Cas, typesystem: TypeSystem, config: C
         cas.get_view( SofaID ).remove_annotation(  par )
 
         
-def annotate_terms_and_definitions(cas: Cas, typesystem: TypeSystem, config: ConfigParser, \
+def annotate_terms_and_definitions(NLP:English, cas: Cas, typesystem: TypeSystem, config: ConfigParser, \
                                    term_and_definition: Tuple[ Tuple[ str, int, int ], Tuple[str, int, int] ]  ):
     
     token_type = config[ 'Annotation' ].get( 'TOKEN_TYPE' )
     defined_type = config[ 'Annotation' ].get( 'DEFINED_TYPE' )
     definition_type = config[ 'Annotation' ].get( 'DEFINITION_TYPE' )
+    lemma_type=config[ 'Annotation' ].get( 'LEMMA_TYPE' )
     SofaID= config[ 'Annotation' ].get( 'SOFA_ID' )
     SCORE=config['TermExtraction'].getfloat( 'TFIDF_WHITELIST' )
 
     Token = typesystem.get_type( token_type )
     Defined = typesystem.get_type( defined_type )
     Sentence = typesystem.get_type( definition_type )
-    
+    Lemma = typesystem.get_type( lemma_type )
+
     term = term_and_definition[0][0]
     term_begin = term_and_definition[0][1]
     term_end = term_and_definition[0][2]
@@ -98,6 +109,8 @@ def annotate_terms_and_definitions(cas: Cas, typesystem: TypeSystem, config: Con
         Defined(begin=term_begin, end=term_end ))
     cas.get_view( SofaID ).add_annotation(
         Sentence(begin=definition_begin, end=definition_end, id='definition' ))
+    cas.get_view( SofaID ).add_annotation(
+        Lemma(begin=term_begin, end=term_end, value=lemmatize( NLP, term ) ))
     
 def clean_term( term ):
     #remove possible enumeration from the term (i.e. 1.1. something ==> something)
@@ -115,3 +128,14 @@ def clean_term( term ):
     term=term.lstrip( "*. " ).strip()
     
     return term
+
+def get_offset_stripped_term( stripped_term, original_text  ):
+    #find offset of stripped term in the original text
+
+    match=re.search(  stripped_term ,  original_text , re.IGNORECASE )
+
+    if match:
+        offset_stripped_term=(match.span()[0], match.span()[1] )
+        return offset_stripped_term
+    else:
+        return None
